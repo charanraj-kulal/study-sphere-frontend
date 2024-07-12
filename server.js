7094dd9fca05bd811819ba7fdc55d7b42db22277
charanraj-kulal <97221926+charanraj-kulal@users.noreply.github.com>
2024-07-12 10:27:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const transporter = nodemailer.createTransport({
-  host: import.meta.env.VITE_SMTP_HOST,
-  port: import.meta.env.VITE_SMTP_PORT,
+  host: process.env.VITE_SMTP_HOST,
+  port: process.env.VITE_SMTP_PORT,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: import.meta.env.VITE_SMTP_USER,
-    pass: import.meta.env.VITE_SMTP_PASS,
+    user: process.env.VITE_SMTP_USER,
+    pass: process.env.VITE_SMTP_PASS,
   },
 });
 const app = express();
